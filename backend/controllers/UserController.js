35c4d6d39cbfe6861f9cb8e208e7d0e275e55838
arosha619 <90087992+arosha619@users.noreply.github.com>
2024-05-13 23:59:51 +0530

    password hashed

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose')
-const user = require('../models/UserModel')
+const mongoose = require("mongoose");
+const User = require("../models/UserModel");
 
-const userLogin = async (req, res)=>{
-    res.json({message:"User login"})
-}
+const userLogin = async (req, res) => {
+  res.json({ message: "User login" });
+};
 
-const userSignup = async (req, res) =>{
-    res.json({message:"User Signup"})
-}
+const userSignup = async (req, res) => {
+  const { email, password } = req.body;
 
-module.exports = { userLogin, userSignup}
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (Error) {
+    res.status(400).json({error:Error.message})
+  }
+};
+
+module.exports = { userLogin, userSignup };
